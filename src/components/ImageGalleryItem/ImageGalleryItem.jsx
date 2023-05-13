8dd9bd3a15d416da79b2c751a650da063aa98e66
olena-nikitchenko <helnikitka@gmail.com>
2023-05-13 20:30:09 +0300

    change ImageGalleryItem  use hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../Modal';
 import css from './ImageGalleryItem.module.css';
 
-class ImageGalleryItem extends Component {
-  state = {
-    shownModal: false,
+const ImageGalleryItem = ({ item }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const onModal = () => {
+    setShowModal(prevState => !prevState);
   };
-  onModal = () => {
-    this.setState(({ shownModal }) => ({ shownModal: !shownModal }));
-  };
-  render() {
-    const { item } = this.props;
-    const { webformatURL } = item;
-    return (
-      <li className={css.Item}>
-        <img
-          onClick={this.onModal}
-          className={css.Image}
-          src={webformatURL}
-          alt="img"
-        />
-        {this.state.shownModal && <Modal onClose={this.onModal} image={item} />}
-      </li>
-    );
-  }
-}
+
+  const { webformatURL } = item;
+
+  return (
+    <li className={css.Item}>
+      <img
+        onClick={onModal}
+        className={css.Image}
+        src={webformatURL}
+        alt="img"
+      />
+      {showModal && <Modal onClose={onModal} image={item} />}
+    </li>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   item: PropTypes.object,
