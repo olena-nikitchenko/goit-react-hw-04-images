049dd97a5c2ea4dcedbadb91f5557450981747c7
olena-nikitchenko <helnikitka@gmail.com>
2023-05-13 22:59:19 +0300

    fixed code

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,5 @@
 import { createPortal } from 'react-dom';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import css from './Modal.module.css';
 
@@ -7,17 +7,16 @@ const ModalRoot = document.querySelector('#ModalRoot');
 
 const Modal = ({ image, onClose }) => {
   useEffect(() => {
+    const onKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
     window.addEventListener('keydown', onKeyDown);
     return () => {
       window.removeEventListener('keydown', onKeyDown);
     };
-  }, []);
-
-  const onKeyDown = e => {
-    if (e.code === 'Escape') {
-      onClose();
-    }
-  };
+  }, [onClose]);
 
   const onOverlayClose = e => {
     if (e.currentTarget === e.target) {
